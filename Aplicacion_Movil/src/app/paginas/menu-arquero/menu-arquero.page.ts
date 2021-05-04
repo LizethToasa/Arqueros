133dd2c979cbaac889afe95a07b525c39d2f6b87
Bryan España <lizeth.toasa@gmail.com>
2021-05-03 21:26:22 -0500

    subir final

--- Aplicacion_Movil/src/app/paginas/menu-arquero/menu-arquero.page.ts
+++ Aplicacion_Movil/src/app/paginas/menu-arquero/menu-arquero.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../servicios/auth.service';
+import { AlertController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-menu-arquero',
   templateUrl: './menu-arquero.page.html',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuArqueroPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authservice : AuthService,
+    private alertCtrl: AlertController,
+    private platform: Platform
+  ) {
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      this.mensajesalir();
+    });
+   }
 
   ngOnInit() {
+  }
+
+  async mensajesalir() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Mensaje',
+      message: '¿Seguro de cerrar sesión?',
+      buttons: [
+       {
+          text: 'Aceptar',
+          handler: () => {
+            this.salir();
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  salir(){ 
+    this.authservice.logout();
   }
 
 }
