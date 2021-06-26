566f2a0f1cd3af62b4c2ca0fbc02b620ab36fa7c
Bryan España <lizeth.toasa@gmail.com>
2021-06-25 22:06:20 -0500

    visualizar avance arquero

--- Aplicacion_Movil/src/app/paginas/arquero-avance/arquero-avance.page.ts
+++ Aplicacion_Movil/src/app/paginas/arquero-avance/arquero-avance.page.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
+import { EntrenadorService } from '../../servicios/entrenador.service';
+import { Avance } from '../../interfaces/avance.interface';
+import { DatosEntrenador } from '../../interfaces/entrenador.interface';
 @Component({
   selector: 'app-arquero-avance',
   templateUrl: './arquero-avance.page.html',
   styleUrls: ['./arquero-avance.page.scss'],
 })
 export class ArqueroAvancePage implements OnInit {
-
-  constructor() { }
+  usuarioId= null;
+  avances: Avance[];
+  entrenadores: DatosEntrenador[];
+  constructor(private AvanceService: EntrenadorService) {
+    this.usuarioId = firebase.auth().currentUser.uid;
+    this.AvanceService.getAvancefecha().subscribe((avance) =>{
+      this.avances = avance.filter(avan=>avan.idarquero == this.usuarioId); 
+      console.log(this.avances);
+    })
+    this.AvanceService.getEntrenadores().subscribe((entrenadores) =>{
+      this.entrenadores = entrenadores;
+    })
+   }
+  
 
   ngOnInit() {
   }
