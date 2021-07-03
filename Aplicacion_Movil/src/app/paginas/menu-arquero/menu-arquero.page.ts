1ac1a5bdc28581c1b2c1569c460fe03f1c20be65
Bryan España <lizeth.toasa@gmail.com>
2021-07-03 18:31:15 -0500

    desactivar usuario

--- Aplicacion_Movil/src/app/paginas/menu-arquero/menu-arquero.page.ts
+++ Aplicacion_Movil/src/app/paginas/menu-arquero/menu-arquero.page.ts
@@ -2,18 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../servicios/auth.service';
 import { AlertController } from '@ionic/angular';
 import { Platform } from '@ionic/angular';
+import * as firebase from 'firebase';
+import { ArqueroService } from '../../servicios/arquero.service';
 @Component({
   selector: 'app-menu-arquero',
   templateUrl: './menu-arquero.page.html',
   styleUrls: ['./menu-arquero.page.scss'],
 })
 export class MenuArqueroPage implements OnInit {
-
+  usuarioId= null;
   constructor(
     private authservice : AuthService,
     private alertCtrl: AlertController,
-    private platform: Platform
+    private platform: Platform,
+    private usuarioService: ArqueroService
   ) {
+    this.usuarioId = firebase.auth().currentUser.uid;
+    this.usuarioService.getArquero(this.usuarioId).subscribe(usuario => {
+      if(usuario.estado == "Inactivo"){
+        this.authservice.logout();
+      }
+
+    });
     this.platform.backButton.subscribeWithPriority(10, () => {
       this.mensajesalir();
     });
